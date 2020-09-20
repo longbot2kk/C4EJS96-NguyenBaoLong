ac8b163be4fcbcab54f212445519705448073fc8
longbot <baolongred2k@gmail.com>
2020-09-21 01:13:33 +0700

    switch

--- Session-3/hwork-4/index.js
+++ Session-3/hwork-4/index.js
@@ -1,24 +1,29 @@
 let list = ["Jeans", "T-Shirt", "Sock"];
-let n = prompt("Hi there, welcome to shop admin panel, what do you want (C, R, U, D)");
-switch(n)
+var n = prompt("Hi there, welcome to shop admin panel, what do you want (C, R, U, D)");
+do{
+    switch(n)
 {
-    case 1:
-        n=`r`;
-        for(let i=1;i<=list.length;i++)
+    case "r":
+        for(let i=0;i<=list.length;i++)
         {
-            console.log(i+"."+list[i]);
+            console.log((i+1)+"."+list[i]);
         }
-    case 2:
-        n=`c`;
+        break;
+    case "c":
+
         let x=prompt("Enter the name of the new item");
-        list.push(i);
-    case 3:
-        n=`u`;
+        list.push(x);
+        break;
+    case "u":
         let m=prompt("Enter the posititon you want to update");
         let u=prompt("Enter the new name: ");
         list[m]=u;
-    case 4:
-        n=`d`;
+        break;
+    case "d":
         let o=prompt("Enter the posititon you want to delete");
         list.splice(o,1);
+        break;
+    default: alert("This command is not supported")
 }
+}
+while(n!=4);
--- Session-3/hwork-4/index1.js
+++ Session-3/hwork-4/index1.js
@@ -1,1 +1,22 @@
-let list =["Jeans", "T-Shirt", "Socks"]
+let list =["Jeans", "T-Shirt", "Socks"];
+var n = prompt("Hi there, welcome to shop admin panel, what do you want (C, R, U, D)");
+if(n="r")
+{
+    for(let i=0;i<=list.length;i++)
+        {
+            console.log((i+1)+"."+list[i]);
+        }
+    
+}
+else if(n="c")
+{
+    let x=prompt("Enter the name of the new item");
+        list.push(x);
+}
+else if(n="u")
+{
+    let m=prompt("Enter the posititon you want to update");
+        let u=prompt("Enter the new name: ");
+        list[m]=u;
+
+}
