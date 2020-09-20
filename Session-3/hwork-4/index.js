d6d983da559ffce8eec56898ae1fba92fc7b2699
longbot <baolongred2k@gmail.com>
2020-09-21 01:22:41 +0700

    fix switch

--- Session-3/hwork-4/index.js
+++ Session-3/hwork-4/index.js
@@ -1,29 +1,31 @@
 let list = ["Jeans", "T-Shirt", "Sock"];
 var n = prompt("Hi there, welcome to shop admin panel, what do you want (C, R, U, D)");
-do{
-    switch(n)
+while(true)
 {
-    case "r":
-        for(let i=0;i<=list.length;i++)
-        {
-            console.log((i+1)+"."+list[i]);
-        }
-        break;
-    case "c":
+    switch(n)
+    {
+        case "r":
+            for(let i=0;i<list.length;i++)
+            {
+                console.log((i+1)+"."+list[i]);
+            }
+            break;
+        case "c":
+    
+            let x=prompt("Enter the name of the new item");
+            list.push(x);
+            break;
+        case "u":
+            let m=prompt("Enter the posititon you want to update");
+            let u=prompt("Enter the new name: ");
+            list[m]=u;
+            break;
+        case "d":
+            let o=prompt("Enter the posititon you want to delete");
+            list.splice(o,1);
+            break;
+        default: alert("This command is not supported")
+    }
+}
+    
 
-        let x=prompt("Enter the name of the new item");
-        list.push(x);
-        break;
-    case "u":
-        let m=prompt("Enter the posititon you want to update");
-        let u=prompt("Enter the new name: ");
-        list[m]=u;
-        break;
-    case "d":
-        let o=prompt("Enter the posititon you want to delete");
-        list.splice(o,1);
-        break;
-    default: alert("This command is not supported")
-}
-}
-while(n!=4);
--- Session-3/hwork-4/index1.js
+++ Session-3/hwork-4/index1.js
@@ -1,6 +1,7 @@
 let list =["Jeans", "T-Shirt", "Socks"];
-var n = prompt("Hi there, welcome to shop admin panel, what do you want (C, R, U, D)");
-if(n="r")
+do{
+    var n = prompt("Hi there, welcome to shop admin panel, what do you want (C, R, U, D)");
+if(n=="r")
 {
     for(let i=0;i<=list.length;i++)
         {
@@ -8,15 +9,25 @@ if(n="r")
         }
     
 }
-else if(n="c")
+else if(n=="c")
 {
     let x=prompt("Enter the name of the new item");
         list.push(x);
 }
-else if(n="u")
+else if(n=="u")
 {
     let m=prompt("Enter the posititon you want to update");
         let u=prompt("Enter the new name: ");
         list[m]=u;
 
 }
+else if(n=="d")
+{
+    let o=prompt("Enter the posititon you want to delete");
+        list.splice(o,1);
+}
+else alert="exit";
+}
+while()
+
+
