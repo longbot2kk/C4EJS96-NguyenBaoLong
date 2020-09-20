4dfeb0d4f40d99b01a4eaa6e82282faad91bbe58
longbot <baolongred2k@gmail.com>
2020-09-21 01:47:20 +0700

    fix

--- Session-3/hwork-4/index.js
+++ Session-3/hwork-4/index.js
@@ -1,6 +1,7 @@
 let list = ["Jeans", "T-Shirt", "Sock"];
 var n = prompt("Hi there, welcome to shop admin panel, what do you want (C, R, U, D)");
-while(true)
+let count = 0;
+while(count<2)
 {
     switch(n)
     {
@@ -10,22 +11,29 @@ while(true)
                 console.log((i+1)+"."+list[i]);
             }
             break;
+            count++
         case "c":
     
             let x=prompt("Enter the name of the new item");
             list.push(x);
             break;
+            count++
         case "u":
             let m=prompt("Enter the posititon you want to update");
             let u=prompt("Enter the new name: ");
             list[m]=u;
+            console.log(list)
             break;
+            count++
         case "d":
             let o=prompt("Enter the posititon you want to delete");
             list.splice(o,1);
             break;
+            count++
         default: alert("This command is not supported")
+        break;
     }
 }
+
     
 
--- Session-3/hwork-4/index1.js
+++ Session-3/hwork-4/index1.js
@@ -1,33 +1,39 @@
 let list =["Jeans", "T-Shirt", "Socks"];
-do{
-    var n = prompt("Hi there, welcome to shop admin panel, what do you want (C, R, U, D)");
-if(n=="r")
+let count=0;
+while(count<2)
+
 {
-    for(let i=0;i<=list.length;i++)
+    var n = prompt("Hi there, welcome to shop admin panel, what do you want (C, R, U, D)");
+
+    if(n=="r")
+{
+    for(let i=0;i<list.length;i++)
         {
             console.log((i+1)+"."+list[i]);
         }
-    
+    count++;
 }
 else if(n=="c")
 {
     let x=prompt("Enter the name of the new item");
         list.push(x);
+        count++;
 }
 else if(n=="u")
 {
     let m=prompt("Enter the posititon you want to update");
         let u=prompt("Enter the new name: ");
         list[m]=u;
-
+        count++;
 }
 else if(n=="d")
 {
     let o=prompt("Enter the posititon you want to delete");
         list.splice(o,1);
+        count++;
 }
 else alert="exit";
 }
-while()
+
 
 
